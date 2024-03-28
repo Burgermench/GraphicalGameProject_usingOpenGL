868877a03c859b35ecb665caf6a12982e4941c37
Romaaa1 <tomasvazroma@gmail.com>
2024-03-28 17:36:04 +0000

    Revert para ontem
    Revert para o estado do code de ontem, colisões, movimentação basica

--- Trabalho_Final_CG/beach_runner.py
+++ Trabalho_Final_CG/beach_runner.py
@@ -1,5 +1,5 @@
 import math
-import pygame as pg
+import pygame
 
 from pygame.locals import *
 from random import randint, choice
@@ -19,6 +19,8 @@ from material.texture import TextureMaterial
 from extras.movement_rig import MovementRig
 
 # Implementation of "Beach Runner" game
+
+
 class Example(Base):
 
     #########################################
@@ -29,7 +31,8 @@ class Example(Base):
         # Meta-info
         self.debug = True   # turn ON or OFF
         self.fps = 60
-        self.frame = 0      # TODO: implement as tuple for logging info to files and/or terminal (with timestamps)
+        # TODO: implement as tuple for logging info to files and/or terminal (with timestamps)
+        self.frame = 0
         self.score = 0      # TODO: implement on bottom-right of screen
         # Rendering, camera and objects
         self.renderer = Renderer()
@@ -41,7 +44,8 @@ class Example(Base):
         self.rig.set_position([0, 2, 25])
         # Sky
         sky_geometry = RectangleGeometry(width=250, height=250)
-        sky_material = TextureMaterial(texture=Texture(file_name="images/sky.jpg"), property_dict={"repeatUV": [5, 5]})
+        sky_material = TextureMaterial(texture=Texture(
+            file_name="images/sky.jpg"), property_dict={"repeatUV": [5, 5]})
         sky = Mesh(sky_geometry, sky_material)
         self.scene.add(sky)
         # Ground and Lanes
@@ -50,35 +54,42 @@ class Example(Base):
         self.lane_spacing: int = 50
         self.lane_switching: bool = False
         self.switch_timer: int = 0
-        self.switch_delay: int = 100  # Adjust the delay time as needed (in milliseconds)
+        # Adjust the delay time as needed (in milliseconds)
+        self.switch_delay: int = 100
         # Render the ground and lanes
-        ground_geometry = RectangleGeometry(width=self.lane_width*self.lane_count+self.lane_spacing*(self.lane_count-1), height=100)
-        ground_material = TextureMaterial(texture=Texture(file_name="images/grass.jpg"), property_dict={"repeatUV": [50, 50]})
+        ground_geometry = RectangleGeometry(
+            width=self.lane_width*self.lane_count+self.lane_spacing*(self.lane_count-1), height=100)
+        ground_material = TextureMaterial(texture=Texture(
+            file_name="images/grass.jpg"), property_dict={"repeatUV": [50, 50]})
         ground = Mesh(ground_geometry, ground_material)
         ground.rotate_x(-math.pi/2)
         ground.set_position([0, -0.5, 0])
         self.scene.add(ground)
         # Render the floor
         floor_geometry = RectangleGeometry(width=15, height=100)
-        floor_material = TextureMaterial(texture=Texture(file_name="images/floor_temple.jpg"))
+        floor_material = TextureMaterial(
+            texture=Texture(file_name="images/floor_temple.jpg"))
         floor = Mesh(floor_geometry, floor_material)
         floor.rotate_x(-math.pi/2)
-        floor.set_position([0, -0.4, 0]) # Adjust the y-position to place the floor on top of the grass
+        # Adjust the y-position to place the floor on top of the grass
+        floor.set_position([0, -0.4, 0])
         self.scene.add(floor)
         # Render the kite
         self.kite_rig = MovementRig()
         self.kite_geometry = MolduraGeometryKite()
-        self.kite_material = TextureMaterial(texture=Texture(file_name="images/fire1.jpg"))  # Placeholder for kite texture
+        self.kite_material = TextureMaterial(texture=Texture(
+            file_name="images/fire1.jpg"))  # Placeholder for kite texture
         self.kite = Mesh(self.kite_geometry, self.kite_material)
         self.kite.set_position([0, 2, 20])  # Initial position of the kite
         self.scene.add(self.kite)
         self.kite_rig.add(self.kite)
-        self.clock: pg.time.Clock = None
-        # Game logic flags
-        self.is_game_paused = False     # TODO:
+        # Set some needed attributes for the game logic
+        self.clock: pygame.time.Clock = None
+        self.is_game_paused = False
         self.is_game_over = False
-        # Granular control
-        self.gravity = 0.2
+        self.fps = 60
+        self.score = 0
+        self.gravity = 0.1
         self.terminal_velocity = 5
         self.jumping = False
         self.sliding = False
@@ -86,34 +97,29 @@ class Example(Base):
         self.jump_height = 4
         self.jump_duration = 60
         self.obstacles = []
-        # Input
-        self.pg_keys = pg.key.get_pressed()
-        self.keys_down = []
-        self.keys_up = []
-        self.keys_released = []
         self.keys_pressed = []
+        self.last_key: str = None
 
     # Starts and loops the game until the game is over
     def run(self):
         self.initialize()
-        self.clock = pg.time.Clock()
-        pg.init()
-        pg.display.set_caption("Beach Runner")
+        self.keys_pressed = pygame.key.get_pressed()
+        self.clock = pygame.time.Clock()
+        pygame.init()
         while not self.is_game_over:
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
                     self.is_game_over = True
-            self.handle_input()
+            self.keys_pressed = pygame.key.get_pressed()
+            self.handle_input(self.keys_pressed)
             self.update()
             self.renderer.render(self.scene, self.camera)
             self.clock.tick(self.fps)
-            pg.display.flip()
-        pg.quit()
+            pygame.display.flip()
+        pygame.quit()
 
     # Updates the game state
     def update(self):
-        self.frame += 1
-        self.check_keys()
         if self.is_game_paused:
             self.clock.tick(0)
         else:
@@ -123,106 +129,55 @@ class Example(Base):
             self.check_collision()
             self.apply_gravity()
             self.score += 1
-            if self.lane_switching and (pg.time.get_ticks() - self.switch_timer) >= self.switch_delay:
+            if self.lane_switching and (pygame.time.get_ticks() - self.switch_timer) >= self.switch_delay:
                 self.lane_switching = False
         else:
             print(f"Game Over! Your _score: {self.score}")
 
-    def handle_input(self):
+    def handle_input(self, keys=None):  # Add 'keys=None' as an argument with default value
+        if keys is None:
+            keys = pygame.key.get_pressed()
+        # Camera movement
+        if keys[K_w]:
+            self.rig.move_forward(0.1)
+        if keys[K_s]:
+            self.rig.move_backward(0.1)
+        if keys[K_a]:
+            self.rig.move_left(0.1)
+        if keys[K_d]:
+            self.rig.move_right(0.1)
         # Kite movement
-        if pg.K_LEFT in self.keys_pressed:  # Move left
-            if self.debug: print("key pressed: Left arrow")
+        if keys[K_LEFT]:  # Move left
             if not self.lane_switching:
                 self.move_to_lane(-2)
                 self.lane_switching = True
-                self.switch_timer = pg.time.get_ticks()
-        if pg.K_RIGHT in self.keys_pressed:  # Move right
-            if self.debug: print("key pressed: Arrow Right")
+                self.switch_timer = pygame.time.get_ticks()
+        if keys[K_RIGHT]:  # Move right
             if not self.lane_switching:
                 self.move_to_lane(2)
                 self.lane_switching = True
-                self.switch_timer = pg.time.get_ticks()
-        
-        not_already_sliding_or_jumping = not self.jumping and not self.sliding # would be redundant
-        if pg.K_UP and not_already_sliding_or_jumping:
-            if self.debug: print("key pressed: Arrow Up")
+                self.switch_timer = pygame.time.get_ticks()
+        if keys[K_UP] and not self.jumping:  # Jump
             self.jumping = True
             self.jump_start_y = self.kite.get_position()[1]
             self.jump_time = 0
-        if pg.K_DOWN and not_already_sliding_or_jumping:  # Slide (TODO: Implement slide)
-            if self.debug: print("key pressed: Arrow Down")
+        if keys[K_DOWN]:  # Slide (TODO: Implement slide)
             self.slide()
-        # Camera movement
-        if pg.K_w in self.keys_pressed:
-            if self.debug: print("key pressed: w")
-            self.rig.move_forward(0.1)
-        if pg.K_s in self.keys_pressed:
-            if self.debug: print("key pressed: s")
-            self.rig.move_backward(0.1)
-        if pg.K_a in self.keys_pressed:
-            if self.debug: print("key pressed: a")
-            self.rig.move_left(0.1)
-        if pg.K_d in self.keys_pressed:
-            if self.debug: print("key pressed: d")
-            self.rig.move_right(0.1)
-        # Activate menu
-        if pg.K_ESCAPE in self.keys_pressed and not self.is_game_paused:
-            if self.debug: print("key pressed: esc, running -> paused")
-            self.is_game_paused = True
-        elif pg.K_ESCAPE and self.is_game_paused:
-            if self.debug: print("key pressed: esc, paused -> running")
+        if keys[K_ESCAPE] and self.is_game_paused:
             self.is_game_paused = False
+        elif keys[K_ESCAPE] and not self.is_game_paused:
+            self.is_game_paused = True
 
     #########################################
     # HELPER FUNCTIONS
 
-    # Collect data on the keys that are down, up, released, and pressed
-    # for more precise, and granular control over input. Instead of only
-    # 'up' and 'down' states, we now also get to manipulate key state
-    # transitions such as:
-    #  (pressed as in key state went from up -> down == [push key attitude])
-    #  (released as in key state going from down -> up == [let go attitude])
-    '''
-    Transitions:
-                ___                     ___
-     ___       |   |        ___        |   |
-    |   |      |   |       |   |       |   |
-    <down>      <up>       <down>       <up>
-       <released>   <pressed>   <released>
-    '''
-    def check_keys(self): # TODO: finish
-        for key, active in enumerate(self.pg_keys):
-            # boolean key states logic simplification
-            key_up_in_previous_frame      = key in self.keys_up
-            key_up_in_current_frame       = not active
-            key_down_in_previous_frame    = key in self.keys_down
-            key_down_in_current_frame     = active
-            key_pressed_in_current_frame  = key_up_in_previous_frame and key_down_in_current_frame
-            key_released_in_current_frame = key_down_in_previous_frame and key_up_in_current_frame
-            # logic application
-            if key_released_in_current_frame:
-                self.keys_released.append(key)
-                if key in self.keys_pressed: self.keys_pressed.remove(key)
-            elif key_pressed_in_current_frame:
-                self.keys_pressed.append(key)
-                if key in self.keys_released: self.keys_released.remove(key)
-            if key_up_in_current_frame and key not in self.keys_up:
-                self.keys_up.append(key)
-                if key in self.keys_down: self.keys_down.remove(key)
-            elif key_down_in_current_frame and key not in self.keys_down:
-                self.keys_down.append(key)
-                if key in self.keys_up: self.keys_up.remove(key)
-            #debug
-            if self.debug and key_pressed_in_current_frame: print(f"key pressed: {key}")
-            
-
-    # Emulate force of gravity over pawn
     def apply_gravity(self):
         kite_pos = self.kite.get_position()
         if self.jumping:
             self.jump_time += 1
             jump_progress = min(self.jump_time / self.jump_duration, 1)
-            new_y = self.jump_start_y + self.jump_height * math.sin(jump_progress * math.pi)
+            new_y = self.jump_start_y + self.jump_height * \
+                math.sin(jump_progress * math.pi)
             self.kite.set_position([kite_pos[0], new_y, kite_pos[2]])
             if self.jump_time >= self.jump_duration:
                 self.jumping = False
@@ -232,7 +187,8 @@ class Example(Base):
 
     def add_obstacle(self):
         obstacle_geometry = MolduraGeometry()
-        obstacle_material = TextureMaterial(texture=Texture(file_name="images/1.jpg"))  # Placeholder for obstacle texture
+        obstacle_material = TextureMaterial(texture=Texture(
+            file_name="images/1.jpg"))  # Placeholder for obstacle texture
         # Randomly select a lane for the obstacle
         obstacle_lane = choice([-1.5, 0, 1.5])
         obstacle_x = obstacle_lane * 2  # Adjust obstacle position based on the lane
@@ -254,7 +210,8 @@ class Example(Base):
             dz = kite_pos[2] - obstacle_pos[2]
             # Check for collision along each axis
             if abs(dx) < kite_radius + obstacle_radius and abs(dy) < kite_radius + obstacle_radius and abs(dz) < kite_radius + obstacle_radius:
-                print(f"Collision detected! Kite position: {kite_pos}, Obstacle position: {obstacle_pos}")
+                print(
+                    f"Collision detected! Kite position: {kite_pos}, Obstacle position: {obstacle_pos}")
                 self.is_game_over = True
                 break  # Exit the loop if a collision is detected
 
@@ -291,9 +248,11 @@ class Example(Base):
             if obstacle.global_position[2] < 30:
                 new_obstacles.append(obstacle)
             else:
-                self.scene.remove(obstacle)  # Remove obstacle from the scene if it's too far
+                # Remove obstacle from the scene if it's too far
+                self.scene.remove(obstacle)
         self.obstacles = new_obstacles
         self.spawn_obstacle()
+
 
 # Instantiate this class and run the program
 Example(screen_size=[1280, 800]).run()
